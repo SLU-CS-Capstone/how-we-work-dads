8dc3d5c75cd5c2cbd9d7f1ca7d1c969a30588462
Rolwes <m235370@academy.usna.edu>
2023-09-11 17:15:08 -0500

    myfirst

--- src/generator.py
+++ src/generator.py
@@ -1,5 +1,7 @@
 from maze import Maze
-
+from pypdf import PdfReader
+page = pdf_writer.add_blank_page(width=8.27 * 72, height=11.7 * 72)
+#making changes to my code
 maze = Maze(20)
 maze.generate_maze()
 maze.print()
